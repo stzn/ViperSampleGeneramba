dc2fe9682afac997c523e05788bbe5217edb6346
stzn <sztk1209@gmail.com>
2018-01-09 18:28:41 +0900

    Collect variables in a struct

--- ViperSampleGeneramba/Models/QiitaItemDTO.swift
+++ ViperSampleGeneramba/Models/QiitaItemDTO.swift
@@ -1,6 +1,19 @@
 import Foundation
 
-struct QiitaItem: Decodable {
+struct QiitaItem: Decodable, Equatable {
+    static func ==(lhs: QiitaItem, rhs: QiitaItem) -> Bool {
+        return lhs.id == rhs.id
+            && lhs.likes_count == rhs.likes_count
+            && lhs.reactions_count == rhs.reactions_count
+            && lhs.comments_count == rhs.comments_count
+            && lhs.title == rhs.title
+            && lhs.created_at == rhs.created_at
+            && lhs.updated_at == rhs.updated_at
+            && lhs.url == rhs.url
+            && lhs.tags == rhs.tags
+            && lhs.user == rhs.user
+    }
+    
     let id: String
     let likes_count: Int
     let reactions_count: Int
@@ -11,11 +24,20 @@ struct QiitaItem: Decodable {
     let url: URL
     let tags: [Tag]
     let user: User?
-    struct Tag: Decodable {
+    
+    struct Tag: Decodable, Equatable {
+        static func ==(lhs: QiitaItem.Tag, rhs: QiitaItem.Tag) -> Bool {
+            return lhs.name == rhs.name
+        }
         let name: String
     }
     
-    struct User: Decodable {
+    struct User: Decodable, Equatable {
+        static func ==(lhs: QiitaItem.User, rhs: QiitaItem.User) -> Bool {
+            return lhs.github_login_name == rhs.github_login_name
+            && lhs.profile_image_url == rhs.profile_image_url
+        }
+        
         let github_login_name: String?
         let profile_image_url: String?
     }
--- ViperSampleGeneramba/Modules/QiitaItems/List/Presenter/QiitaItemsListPresenter.swift
+++ ViperSampleGeneramba/Modules/QiitaItems/List/Presenter/QiitaItemsListPresenter.swift
@@ -1,33 +1,68 @@
 import UIKit
 
+
 final class QiitaItemsListPresenter {
 
-    // MARK: - Private properties -
-    
-    private weak var _view: QiitaItemsListViewInterface?
-    private var _interactor: QiitaItemsListInteractorInterface
-    private var _wireframe: QiitaItemsListWireframeInterface
-
-    private var _trigger: TriggerType = TriggerType.refresh
-    private var _state: NetworkState = NetworkState.nothing
-    private var nextPage: Int = 1
-    
-    var canLoad: Bool {
-        let loadingState: [NetworkState] = [.requesting, .reachedBottom]
-        return !loadingState.contains(_state)
+    struct State: Equatable {
+        
+        let trigger: TriggerType
+        let state: NetworkState
+        let nextPage: Int
+        let contents: [QiitaItem]
+        
+        init(trigger: TriggerType = .refresh,
+             state: NetworkState = .nothing,
+             nextPage: Int = 1,
+             contents: [QiitaItem] = []) {
+            
+            self.trigger = trigger
+            self.state = state
+            self.nextPage = nextPage
+            self.contents = contents
+        }
+        
+        var canLoad: Bool {
+            let loadingState: [NetworkState] = [.requesting, .reachedBottom]
+            return !loadingState.contains(state)
+        }
+        
+        static func ==(lhs: QiitaItemsListPresenter.State, rhs: QiitaItemsListPresenter.State) -> Bool {
+            return lhs.state == rhs.state
+                && lhs.nextPage == rhs.nextPage
+                && lhs.trigger == rhs.trigger
+                && lhs.contents == rhs.contents
+        }
+        
+        static let initial = State()
     }
     
-    private var contents: [QiitaItem] = [] {
+    
+    
+    // MARK: - Private properties -
+    
+    private var state: State = State.initial {
         didSet {
-            if contents.count > 0 {
+            if oldValue == state {
+                return
+            }
+            
+            if oldValue.contents == state.contents {
+                return
+            }
+            
+            if state.contents.count > 0 {
                 _view?.showQiitaItemsList()
             } else {
-                if _state != .requesting {
+                if state.state != .requesting {
                     _view?.showNoContentScreen()
                 }
             }
         }
     }
+    
+    fileprivate weak var _view: QiitaItemsListViewInterface?
+    private var _interactor: QiitaItemsListInteractorInterface
+    private var _wireframe: QiitaItemsListWireframeInterface
 
     // MARK: - Lifecycle -
 
@@ -44,77 +79,86 @@ extension QiitaItemsListPresenter: QiitaItemsListPresenterInterface {
 
     func viewDidLoad() {
         
-        if !canLoad { return }
+        if !state.canLoad { return }
         
-        _state = .requesting
-        _trigger = .refresh
+        
+        state = State(trigger: .refresh, state: .requesting)
         _interactor.fetchList(query: "", page: 1)
         _view?.showLoading()
     }
     
     func searchBarTextDidChange(text: String) {
         
-        if !canLoad { return }
+        if !state.canLoad { return }
         
-        _state = .requesting
-        _trigger = .searchTextChange
-        nextPage = 1
-        contents = []
-        _interactor.fetchList(query: text, page: nextPage)
+        
+        state = State.init(trigger: .searchTextChange,
+                           state: .requesting,
+                           nextPage: 1,
+                           contents: [])
+        _interactor.fetchList(query: text, page: state.nextPage)
         _view?.showLoading()
     }
     
     func loadMore(searchText text: String) {
         
-        if !canLoad { return }
+        if !state.canLoad { return }
         
-        _state = .requesting
-        _trigger = .loadMore
-        _interactor.fetchList(query: text, page: nextPage)
+        state = State.init(trigger: .loadMore,
+                           state: .requesting,
+                           nextPage: state.nextPage,
+                           contents: state.contents)
+        _interactor.fetchList(query: text, page: state.nextPage)
         _view?.showLoading()
     }
     
     func didSelectRowAt(_ indexPath: IndexPath) {
         
-        guard contents.count > indexPath.row else { return }
+        guard state.contents.count > indexPath.row else { return }
         
-        let item = contents[indexPath.row]
+        let item = state.contents[indexPath.row]
         _wireframe.showDetail(item: item)
     }
     
     func numberOfRows() -> Int {
-        return contents.count
+        return state.contents.count
     }
     
     func itemAt(_ indexPath: IndexPath) -> QiitaItem? {
         
-        guard contents.count > indexPath.row else { return nil }
+        guard state.contents.count > indexPath.row else { return nil }
         
-        return contents[indexPath.row]
+        return state.contents[indexPath.row]
     }
 }
 
 extension QiitaItemsListPresenter: QiitaItemsListInteractorOutputInterface {
     func fetchedList(items: [QiitaItem]) {
         
-        _state = .nothing
+        var networkState: NetworkState = .nothing
         _view?.hideLoading()
-        contents += items
-        nextPage += 1
         
-        if contents.count != 0 && items.count == 0 {
-            _state = .reachedBottom
+        if state.contents.count != 0 && items.count == 0 {
+            networkState = .reachedBottom
         }
         
-        if _trigger == .searchTextChange {
+        state = State.init(trigger: state.trigger,
+                           state: networkState,
+                           nextPage: state.nextPage + 1,
+                           contents: state.contents + items)
+
+        if state.trigger == .searchTextChange {
             _view?.scrolltoTop()
         }
+        
     }
     
     func fetchedListError(error: Error) {
-        _state = .nothing
-        contents = []
-        nextPage = 1
+
+        state = State.init(trigger: state.trigger,
+                           state: .nothing,
+                           nextPage: 1,
+                           contents: [])
         _view?.hideWithError(message: error.localizedDescription)
     }
 }
